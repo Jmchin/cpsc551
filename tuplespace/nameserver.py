1db308980f049ce516802c4adb0d7a1f2e6d0875
Justin Chin <justinmichaelchin@gmail.com>
2019-11-23 12:39:36 -0800

    builds a new tuple of form ("users", {bindings})
    Before we were naively writing out name-address bindings in their own
    individual tuples of the form (name, event, binding). This change
    leaves those naive writes, but adds an additional 2-tuple where all
    the users bindings are stored in a dictionary. This dictionary can be
    accessed quickly by matching on a 2-tuple ("users", None) and returned
    to the client.

--- tuplespace/nameserver.py
+++ tuplespace/nameserver.py
@@ -26,6 +26,17 @@ MAX_UDP_PAYLOAD = 65507
 
 def main(address, port):
 
+    def notif_to_dict(notification):
+        """Converts a notification decoded from the network into a dictionary"""
+        notification = notification.replace(" ", ",", 2) # comma separate our three fields
+        notification = notification.split(",", 2)
+
+        return { "name": notification[0],
+                 "event": notification[1],
+                 "message": notification[2]
+        }
+
+
     server_address = ('', int(port))
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -41,23 +52,48 @@ def main(address, port):
 
     try:
 
+        # TODO: Listen for incoming connections from the mblog client
+
+        # The nameserver needs to accept an incoming connection from
+        # the client, where the message requests an address binding
+        # for a username in the microblog group.
+
+        # Once the request is received, the nameserver should look up
+        # the binding for the name, and if it exists, return it to the
+        # client. A list of all other bindings should also be returned
+        # to the client so that the tuplespace operation is played to
+        # everyone.
+
+        # connect to our tuplespace
         ts = proxy.TupleSpaceAdapter("http://localhost:8001")
 
         while True:
-            # listen for start events
             data, _ = sock.recvfrom(MAX_UDP_PAYLOAD)
             notification = data.decode()
-            notif_lst = notification.split()
+            notif_dict = notif_to_dict(notification)
 
-            # write start and adapter events to tuplespace
-            if "start"   in notif_lst:
+            if notif_dict["event"] == "start":
                 # first, remove the existing binding
-                ts._inp((notif_lst[0], "start", str))
+                ts._inp((notif_dict["name"], "start", str))
                 # second, add the new binding
-                ts._out(notif_lst)
-            if "adapter" in notif_lst:
-                ts._inp((notif_lst[0], "adapter", str))
-                ts._out(notif_lst)
+                ts._out((notif_dict["name"], "start", notif_dict["message"]))
+
+            if notif_dict["event"] == "adapter":
+                # get the users tuples
+                users = ts._inp(("users", None))
+                # create the users dict if it does not exist
+                users = users if users else ("users", dict())
+                # build the binding
+                bindings = users[1]
+                # set key-value pair, {name : message}
+                bindings[notif_dict["name"]] = notif_dict["message"]
+                # write the new bindings list to the tuplespace
+                ts._out(("users", bindings))
+
+                #TODO: probably need to remove this, these bindings
+                # not needed
+                ts._inp((notif_dict["name"], notif_dict["event"], str))
+                ts._out((notif_dict["name"], notif_dict["event"], notif_dict["message"]))
 
     except Exception as e:
         print(e)
