cf93b6f0a1046f4f8dd2ff012a7f997a47af673a
DANIEL MIRANDA <dvspirate@csu.fullerton.edu>
2019-12-11 14:00:13 -0800

    Implementing log and log entry functiionality. TODO: Implementation to distinguish between commited and non-commited log entries

--- raft.py
+++ raft.py
@@ -115,6 +115,11 @@ class Vote:
     pass
 
 
+class Operation(Enum):
+    take = 0
+    write = 1
+
+
 # TODO
 class LogEntry:
     """ An entry in our replicated, distributed log
@@ -123,17 +128,29 @@ class LogEntry:
     replicated to the StateMachine
 
     """
-    pass
-
+    def __init__(self, term, operation, payload):
+        self.current_term = term
+        self.event_type = Operation(operation)
+        self.payload = payload
 
 # TODO
 class Log:
     """ The replicated, distributed log
 
     """
-    pass
+    def __init__(self):
+        self.log_list = []
+        self.commited = False
+        self.next_available_index = 0
 
+    def append(self, logEntry):
+        self.log_list.append(logEntry)
+        self.next_available_index += 1
 
+    def display(self):
+        for le in self.log_list:
+            print(f'Term:{le.current_term}\n Event:{le.event_type}\n Payload:{le.payload}')
+            print('\n')
 # TODO
 class Snapshot:
     """ A complete system state for a range of log entries
